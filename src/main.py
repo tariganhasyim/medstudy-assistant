5bbcf25dcc5310ecbe2ef163a6b776ca0c7c426c
tariganhasyim <hasyimtarigan0212@gmail.com>
2026-08-05 13:10:20 +0700

    Add initial application structure

---
+++ src/main.py
@@ -0,0 +1,19 @@
+"""
+Main application entry point.
+
+Project:
+MedStudy Assistant
+
+Author:
+Muhammad Hasyim Ashari
+
+Status:
+Under Development
+"""
+
+def main():
+    print("MedStudy Assistant")
+
+
+if __name__ == "__main__":
+    main()
